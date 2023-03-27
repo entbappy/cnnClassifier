3b39f68f12e4ebdf794b61c4359965de42dab592
entbappy <entbappy73@gmail.com>
2023-03-27 19:12:21 +0600

    updated

--- src/cnnClassifier/components/__init__.py
+++ src/cnnClassifier/components/__init__.py
@@ -1,3 +1,4 @@
 from cnnClassifier.components.data_ingestion import DataIngestion
 from cnnClassifier.components.prepare_base_model import PrepareBaseModel
 from cnnClassifier.components.prepare_callback import PrepareCallback
+
